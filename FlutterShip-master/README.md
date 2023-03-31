02f47952864b424226aae79495b7763a3118a260
praveen2221 <praveenap2221@gmail.com>
2023-03-31 10:26:27 +0530

    first commit

---
+++ FlutterShip-master/README.md
@@ -0,0 +1,5 @@
+Check the web app made with flutter which displays the UI design codes online.
+Web App: https://fluttershipp.web.app
+
+
+![image](https://user-images.githubusercontent.com/85046336/204139441-2b11bfe6-2803-46f0-bc55-18ed04b94cc3.png)
---
+++ FlutterShip-master/lib/Constants/Assets.dart
@@ -0,0 +1,15 @@
+part of './Constants.dart';
+
+class Assets {
+  static const String google = 'assets/google.png';
+  static const String carTire = 'assets/carTire.png';
+  static const String dribbbleBal = 'assets/dribbbleBal.png';
+}
+
+class TeslaAssets {
+  static const String car = 'assets/Tesla/Car.png';
+  static const String body = 'assets/Tesla/Body.png';
+  static const String lights = 'assets/Tesla/Lights.png';
+  static const String readyMade = 'assets/Tesla/ReadyMade.png';
+  static const String headLightIcon = 'assets/Tesla/HeadLightIcon.png';
+}
